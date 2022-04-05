ce4f533c1a3e97e269ccb386d82abf16d5a7cb70
hisyambagasprakoso <hisyambagasprakoso@gmail.com>
2022-04-05 10:59:46 +0700

    Syam

---
+++ src/main/java/com/domain/controllers/HomeController.java
@@ -0,0 +1,6 @@
+package com.domain.controllers;
+
+public class HomeController {
+    
+    
+}
--- src/main/java/com/domain/controllers/WelcomeController.java
+++ src/main/java/com/domain/controllers/WelcomeController.java
@@ -1,15 +1,13 @@
 package com.domain.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/welcome")
-
 public class WelcomeController {
 
-    @GetMapping
+    @RequestMapping("/")
     public String welcome(){
         return "Welcome To Spring Boot Rest API";
     }
